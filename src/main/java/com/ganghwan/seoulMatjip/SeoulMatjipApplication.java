78f121861bb7c455047c5e38c1d7361939e75e95
ghks <whrudghks22@naver.com>
2022-02-24 21:08:03 +0900

    [조경환]  - 생성

---
+++ src/main/java/com/ganghwan/seoulMatjip/SeoulMatjipApplication.java
@@ -0,0 +1,13 @@
+package com.ganghwan.seoulMatjip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulMatjipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulMatjipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganghwan/seoulMatjip/SeoulMatjipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganghwan.seoulMatjip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulMatjipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
