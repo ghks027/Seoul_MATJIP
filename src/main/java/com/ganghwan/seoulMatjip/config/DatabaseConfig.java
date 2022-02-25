be82df858c35b471e875a6d6d76ac325027bccd2
ghks <whrudghks22@naver.com>
2022-02-25 20:58:51 +0900

    ghks027/Seoul_MATJIP#1 [조경환]  - 환경 세팅

---
+++ src/main/java/com/ganghwan/seoulMatjip/config/DatabaseConfig.java
@@ -0,0 +1,29 @@
+package com.ganghwan.seoulMatjip.config;
+
+import java.io.IOException;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+@MapperScan(basePackages = "com.ganghwan.seoulMatjip.*")
+public class DatabaseConfig {
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+		sessionFactory.setMapperLocations(res);
+		
+		return sessionFactory.getObject();
+	}
+}
